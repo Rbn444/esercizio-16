4a9088aba1c341156ef6b1dabb096ae7403d6a4f
Rbn444 <106075599+Rbn444@users.noreply.github.com>
2022-06-23 16:20:31 +0200

    Add files via upload

---
+++ epic esercizi 16/js.js
@@ -0,0 +1,94 @@
+/*menù*/
+
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+  /*button formattazione*/
+let h1 = document.querySelector("h1");
+let buttonBig = document.querySelector("#grande");
+let buttonColor = document.querySelector("#colore");
+let buttonUp = document.querySelector("#maiuscolo");
+let buttonHide = document.querySelector("#nascondi");
+
+
+
+buttonBig.addEventListener("click", function() {
+    h1.classList.toggle("big")
+})
+buttonColor.addEventListener("click", function() {
+    h1.classList.toggle("colore")
+})
+buttonUp.addEventListener("click", function() {
+    h1.classList.toggle("upcase")
+})
+buttonHide.addEventListener("click", function() {
+    h1.classList.toggle("hide")
+})
+
+  /*check list*/
+
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
+
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
+
+function newElement() {
+    var li = document.createElement("li");
+    var inputValue = document.getElementById("myInput").value;
+    var t = document.createTextNode(inputValue);
+    li.appendChild(t);
+    if (inputValue === '') {
+      alert("You must write something!");
+    } else {
+      document.getElementById("myUL").appendChild(li);
+    }
+    document.getElementById("myInput").value = "";
+  
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    li.appendChild(span);
+  
+    for (i = 0; i < close.length; i++) {
+      close[i].onclick = function() {
+        var div = this.parentElement;
+        div.style.display = "none";
+      }
+    }
+  }
